c91fca611bb8001824f455472854fedcc44a9a22
Clemens Baumgartner <clemens.baumgartner@scb.co.at>
2022-11-03 09:54:30 +0100

    - remove preview nuget packages - update nugets

---
+++ FaulandCc.MAUI.GesturePatternView/Extensions/MauiAppBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using SkiaSharp.Views.Maui.Controls.Hosting;
+
+namespace FaulandCc.MAUI.GesturePatternView.Extensions;
+
+public static class MauiAppBuilderExtensions
+{
+    public static MauiAppBuilder UseFaulandGesturePattern(this MauiAppBuilder self)
+    {
+        return self.UseSkiaSharp();
+    }
+}
---
+++ GesturePatternView.Maui__/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GesturePatternView.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
